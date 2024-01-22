dfa66d39e5872e506f473b10cf3aec1d98372398
alibek <alibek1024@mail.ru>
2024-01-22 17:45:45 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,7 @@
+let burger = document.querySelector('.burger')
+let burgerBtn = document.querySelector('.burger__btn')
+
+let toggleBurger = ()=>{
+    burger.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+}
